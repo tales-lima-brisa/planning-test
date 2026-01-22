e66a852a7cf015cd220d12a5785f1db781f3d662
Tales de Lima Filho <tales.lima@grupobrisanet.com.br>
2026-01-22 13:27:39 -0300

    return to render

--- services/socketService.ts
+++ services/socketService.ts
@@ -27,7 +27,7 @@ class SocketService {
     // If no protocol is provided, default to wss:// (Secure) instead of ws://
     // This fixes the "insecure WebSocket connection" error on Vercel/HTTPS.
     if (!url.startsWith("ws://") && !url.startsWith("wss://")) {
-      url = `ws://${url}`;
+      url = `wss://${url}`;
     }
 
     return new Promise((resolve, reject) => {
