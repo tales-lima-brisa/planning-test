2466b71498c6bf8332eb808a8884a8bc3824f7e2
Tales de Lima Filho <tales.lima@grupobrisanet.com.br>
2026-01-22 12:10:47 -0300

    change to aws

--- App.tsx
+++ App.tsx
@@ -22,7 +22,7 @@ import { TaskList } from "./components/TaskList";
 const uuid = () => Math.random().toString(36).substring(2, 9);
 
 // HARDCODED PRODUCTION SERVER
-const SERVER_URL = "planning-test-vf5a.onrender.com";
+const SERVER_URL = "https://3.144.103.157:8080";
 
 // Helper for finding closest Fibonacci
 const getClosestFibonacci = (num: number): string | number => {
